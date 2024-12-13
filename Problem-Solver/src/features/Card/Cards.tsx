c7990793ba5c6ee3ebab680102294e69523f0d80
mobin afshari <mma137813781378@gmail.com>
2024-12-13 19:01:31 +0330

    no card found paragraph added

--- Problem-Solver/src/features/Card/Cards.tsx
+++ Problem-Solver/src/features/Card/Cards.tsx
@@ -3,9 +3,10 @@ import Card from "./Card"
 import "./styles/cards.scss";
 
 function Cards() {
-  const {cards} = useMainContext()
+  const {cards} = useMainContext();
+  if(cards?.length === 0) return <p style={{textAlign : "center" , color : "gray"}}>You don't have any credit card yet.</p>;
   return (
-    <section className="cards">{cards?.map((card) => <Card card={card} key={card.card}/>)}</section>
+    <section className="cards">{cards?.map((card) => <Card card={card} key={card.cardNumber}/>)}</section>
   )
 }
 
